a30445a73a607d36719497169bc09dda32df03c4
JuanPabloRobledo <juanpablorobledo1@hotmail.com>
2023-06-25 18:28:31 -0300

    h

---
+++ js/java.js
@@ -0,0 +1,30 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+window.addEventListener("scroll", function () {
+    var nav = document.querySelector("header");
+    nav.classList.toggle("abajo", window.scrollY > 0);
+})
+
+window.addEventListener("load", function () {
+    this.document.getElementById("loader").classList.toggle("loader2")
+})
+
+window.sr = ScrollReveal();
+
+sr.reveal('.', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-500px'
+});
+
+
