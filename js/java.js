e72edef97994dfcf536f3a696f3b75c2273947dc
JuanPabloRobledo <juanpablorobledo1@hotmail.com>
2023-07-11 17:27:00 -0300

    h

--- js/java.js
+++ js/java.js
@@ -19,6 +19,17 @@ window.addEventListener("load", function () {
     this.document.getElementById("loader").classList.toggle("loader2")
 })
 
+const btnEnvio = document.getElementById('enviarCorreo');
+btnEnvio.addEventListener('click', function (e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const nombre = document.getElementById('nombre').value;
+    const mensaje = document.getElementById('mensaje').value;
+    const asunto = document.getElementById('asunto').value;
+    window.location.href = `mailto:raraoficialcontacto@gmail.com?
+subject=Consulta&body=Nombre%3A${nombre}%0AEmail%3A${email}%0AAsunto%3A${asunto}%0AMensaje%3A${mensaje}`;
+});
+
 window.sr = ScrollReveal();
 
 sr.reveal('.tituloEmisoras', {
